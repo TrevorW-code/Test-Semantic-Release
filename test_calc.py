46dc5192c2eaa030b11a53211a9f4915ec33f452
TrevorW-code <trwa4172@colorado.edu>
2022-05-30 13:46:10 -0600

    test: made test oneline

--- test_calc.py
+++ test_calc.py
@@ -4,8 +4,7 @@ import calc # can import because same directory
 class TestCalc(unittest.TestCase):
 
 	def test_add(self):
-		result = calc.add(10,5)
-		self.assertEqual(result,15)
+		self.assertEqual(calc.add(10,5),15)
 
 if __name__ == '__main__':
 	unittest.main()
