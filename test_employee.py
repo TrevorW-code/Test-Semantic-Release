40f0b20edcc91385bf4ed485410db932a37e520a
TrevorW-code <trwa4172@colorado.edu>
2022-05-30 17:06:06 -0600

    feat(employee): created monthly schedule method and following tests

--- test_employee.py
+++ test_employee.py
@@ -1,8 +1,18 @@
 import unittest
+from unittest.mock import patch
 from employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # good for doing something one time (training ml/populate database)
+        pass 
+
+    @classmethod
+    def tearDownClass(cls):
+        pass # good for doing something one time
 
     def setUp(self):
         self.emp_1 = Employee('Corey', 'Schafer', 50000)
@@ -43,6 +53,20 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_1.pay, 52500)
         self.assertEqual(self.emp_2.pay, 63000)
 
+    def test_monthly_schedule(self):
+        with patch('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.emp_1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/Schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp_2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/Smith/June')
+            self.assertEqual(schedule, 'Bad Response!')
 
 if __name__ == '__main__':
     unittest.main()
